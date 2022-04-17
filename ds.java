74d5f48516104648348e57f000ed5dac4707565c
rakib-shahid <rakibshahid1298@gmail.com>
2022-04-17 18:49:04 -0400

    got serials

--- ds.java
+++ ds.java
@@ -3,30 +3,40 @@ import java.util.*;
 
 public class ds{
     public static void main(String[] args) throws IOException{
+        //take drive letter of SD Card from user
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter drive letter: ");
         String driveLetter = scan.nextLine();
         scan.close();
 
+        //reads files in default rom location
         File folder = new File(driveLetter+":/roms/nds");
-        File[] listOfFiles = folder.listFiles();
+        File[] fileList = folder.listFiles();
 
+        //arrays for storing serial and file names
+        ArrayList<String> titles = new ArrayList<String>();
         ArrayList<String> serials = new ArrayList<String>();
 
-        for (File file : listOfFiles) {
+        for (File file : fileList) {
             if (file.isFile()) {
+                titles.add(file.getName());
+
+                //reads serial from nds
                 FileInputStream fileIn = new FileInputStream(file.toPath().toString());
                 fileIn.skip(12);
                 String gameSerial = "";
                 for (int i = 0; i < 4; i++){
                     gameSerial += (char)fileIn.read();
                 }
+
                 serials.add(gameSerial);
             }
         }
-        
-        for (String gameSerial : serials){
-            System.out.println(gameSerial);
+
+
+        //print to terminal (for testing)
+        for (int i = 0; i < serials.size(); i++){
+            System.out.println("Serial = " + serials.get(i) + "\nFile Name = " + titles.get(i) + "\n");
         }
         
 
